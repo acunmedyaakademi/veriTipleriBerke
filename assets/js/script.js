e411026058007534957e290efca2ca505edbac63
[Berke <berkekeskinnokta17@gmail.com>
2024-05-23 22:10:55 +0300

    ödev bitti

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+// String: MEtinsel ifadeleri temsil eder.
+const string = "Merhaba dünya";
+console.log(string);
+
+// Number: İşlem yapabilen sayıları temsil eder.
+const number = 42;
+console.log(number);
+
+// Boolean: true veya false sorgularını temsil eder.
+const boolean = true;
+console.log(boolean);
+
+// Null: Boş değeri temsiş eder.
+const bosDeger = null;
+console.log(bosDeger);
+
+// Undefined: Tanımlanmamış değeri temsil eder.
+let tanimlanmamis;
+console.log(tanimlanmamis);
+
+// Object: Karmaşık veri yapılarını ve fonksiyonları temsil eder. Json formatında da oluşturulabilir.
+const kullanici = {
+    ad: "John",
+    soyad: "Doe",
+    yas: 30
+  };
+  console.log(kullanici);
+
+// Array: Diziye dizilmiş bir dizi öğeyi temsil eder. Json formatında da oluşturulabilir.
+const dizi = [1, 2, 3, 4, 5];
+console.log(dizi);
+
+// Symbol: Benzersiz ve değişmez veri türüdür.
+const symbol = Symbol("benzersiz sembol");
+console.log(symbol);
+
+// BigInt: Büyükk işlemler ve aritmetik işlemler yapmak için büyük tam sayıları temsil eder.
+const bigInt = 1234567890123456789012345678901234567890n;
+console.log(bigInt);
+
